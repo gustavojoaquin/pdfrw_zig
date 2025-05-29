fe37173a41c49123dcf4c0ff740191df02e2eada
Gustavo JCL <gjcalizayal@unjbg.edu.pe>
2025-05-29 17:42:24 -0500

    fix: test import in test_objects.zig

--- src/objects/mod_tests.zig
+++
@@ -1,4 +0,0 @@
-test "test objects" {
-    _ = @import("test/test_pdfobject.zig");
-    _ = @import("test/test_pdfname.zig");
-}
--- src/objects/test_objects.zig
+++ src/objects/test_objects.zig
@@ -1,2 +1,8 @@
+const std = @import("std");
+
 pub const test_pdfobject = @import("test/test_pdfobject.zig");
 pub const test_pdfname = @import("test/test_pdfname.zig");
+
+test {
+    std.testing.refAllDecls(@This());
+}
